b7c6ff98ee0c1db96c891b506406fa335df3f246
mvajhi <mvajhimv@gmail.com>
2025-01-14 23:17:12 +0330

    Fix config and added ansible files

--- 2-ansible/bootstrap.sh
+++ 2-ansible/bootstrap.sh
@@ -3,5 +3,5 @@
 # chage the apt source to iran
 sed -i 's/http:\/\/us./http:\/\/ir./g' /etc/apt/sources.list
 
-sudo apt update
-sudo apt install -y vim tmux
+# sudo apt update
+# sudo apt install -y vim tmux
