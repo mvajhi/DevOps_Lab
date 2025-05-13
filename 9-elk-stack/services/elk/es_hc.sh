4e2a2cf477baa39696ef97639f826c46f707a6cb
mvajhi <mvajhimv@gmail.com>
2025-05-13 04:01:28 +0330

    Add configuration templates and Docker Compose files for ELK stack services

---
+++ 9-elk-stack/services/elk/es_hc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# https://github.com/AhmadRafiee/DevOps_Certification
+set -eo pipefail
+
+if health="$(curl -fsSL "http://$ELASTIC_USERNAME:$ELASTIC_PASSWORD@localhost:9200/_cat/health?h=status")"; then
+	health="$(echo "$health" | sed -r 's/^[[:space:]]+|[[:space:]]+$//g')" # trim whitespace (otherwise we'll have "green ")
+	if [ "$health" = 'green' ] || [ "$health" = "yellow" ]; then
+		exit 0
+	fi
+	echo >&2 "unexpected health status: $health"
+fi
+
+exit 1
